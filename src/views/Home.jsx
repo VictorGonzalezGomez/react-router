98324a1f880fc6fe18ab30f063fa0c95bb3c51e5
victor gonzalez <v.gonzalezgomez.vg@gmail.com>
2022-09-24 16:57:30 -0300

    adding alt to every image

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div className="customFlexContainer">
           <h1>Bienvenido a Happy Cake</h1>
-          <img variant="top" src={cakeImg} className="customImgHome"/>
+          <img variant="top" src={cakeImg} alt="dibujo de un pastel con una frutilla en la parte superior de color que asimila al chocolate y una franja roja en medio" className="customImgHome"/>
     </div>
   );
 }
